8c2f45ff9c3dc3dcee40b5c129874e56cbe38ad2
Caleb Rogers <caleb@calebjay.com>
2024-03-19 15:05:23 +0800

    Don't run ts on config files lol

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import nightwatchPlugin from 'vite-plugin-nightwatch'
 const viteEnv = {}
 Object.keys(process.env).forEach((key) => {
   if (key.startsWith(`VITE_`)) {
+    // @ts-ignore
     viteEnv[`import.meta.env.${key}`] = process.env[key]
   }
 })
