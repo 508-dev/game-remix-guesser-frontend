948fec6bc864d2142c9128d3a03043e397127fa4
Caleb Rogers <caleb@calebjay.com>
2024-03-19 14:58:36 +0800

    Ensure real env variables are seen by vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
 
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key]
+  }
+})
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     nightwatchPlugin(),
   ],
+  define: viteEnv,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
